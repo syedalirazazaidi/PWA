dcd9d3556fdb33974c898f09c3adfd2dce5695b6
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2020-08-11 18:57:59 +0500

    updates

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -13,6 +13,7 @@ const self = this;
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
+      console.log(cache, "files");
       return cache.addAll(urlsToCache);
     })
   );
