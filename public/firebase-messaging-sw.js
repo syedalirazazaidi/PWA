21cc0ae3f81cbbadc24ebd23d1042571b1e29854
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2020-08-12 01:02:57 +0500

    added

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -17,7 +17,13 @@ if ("serviceWorker" in navigator) {
 }
 
 firebase.initializeApp({
+  apiKey: "AIzaSyBNXZPiZhhGYOpZUUDY3vk_UBCzPlP-i4M",
+  authDomain: "expense-tracker-f0f17.firebaseapp.com",
+  projectId: "expense-tracker-f0f17",
+  storageBucket: "expense-tracker-f0f17.appspot.com",
   messagingSenderId: "951718115688",
+  appId: "1:951718115688:web:edc6057575d705c25b7edd",
+  measurementId: "G-ZCC7VCSHNC",
 });
 
 const initMessaging = firebase.messaging();
